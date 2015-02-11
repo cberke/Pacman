cd22538c1722d12ef35584ce2874a5f2fb4b40dc
jackphillips7764 <jackiscool20@gmail.com>
2015-02-11 11:26:30 -0500

    Update
    Cole is stupid

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.awt.event.KeyListener;
 import javax.swing.JTextField;
 
 
-public class Main implements KeyListener {
+public class Main {
 	
 	// 0 = dot
 	// 1 = wall
@@ -57,69 +57,13 @@ public class Main implements KeyListener {
 
 			
 	};
-	public static DrawingPanel panel;
-	public static Graphics g;
-	
-	public static boolean isGaming = true; 
-	public static int [] pacPosition = {0,0};
+
+
 	
 	public static void main(String args[]){
-		
-		Main main = new Main(); //get rid of
-		main.setUP();	
-		
-		//while(isGaming){
-			
-		//}
-		
-	}public void setUP(){
-		panel.addKeyListener(this);
-	}
-
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		//System.out.println(arg0);
-		if(arg0.getKeyCode() == 39){
-			if(board[pacPosition[0]][pacPosition[1]+1] != 1){
-				board[pacPosition[0]][pacPosition[1]] = 2;
-				board[pacPosition[0]][pacPosition[1]+1] = 3;
-				display2(g, boxH, boxW);
-			}
-		}
-		else if(arg0.getKeyCode() == 40){
-			if(board[pacPosition[0]+1][pacPosition[1]] != 1 && board[pacPosition[0]+1][pacPosition[1]] != 8){
-				board[pacPosition[0]][pacPosition[1]] = 2;
-				board[pacPosition[0] +1][pacPosition[1]] = 3;
-				display2(g, boxH, boxW);	
-			}
-		}
-		else if(arg0.getKeyCode() == 37){
-			if(board[pacPosition[0]][pacPosition[1]-1] != 1){
-				board[pacPosition[0]][pacPosition[1]] = 2;
-				board[pacPosition[0]][pacPosition[1]-1] = 3;
-				display2(g, boxH, boxW);	
-			}
-		}
-		else if(arg0.getKeyCode() == 38){
-			if(board[pacPosition[0]-1][pacPosition[1]] != 1){
-				board[pacPosition[0]][pacPosition[1]] = 2;
-				board[pacPosition[0]-1][pacPosition[1]] = 3;
-				display2(g,boxH , boxW);	
-			}
-		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		//System.out.println(e);
+		Manager game = new Manager();
+		//game.play();
 		
 	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	
 }
