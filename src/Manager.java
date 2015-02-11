77cb9da2f19aafc25ae27685509de935076e8f29
jackphillips7764 <jackiscool20@gmail.com>
2015-02-11 13:29:16 -0500

    Update Manager.java

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -8,17 +9,59 @@ import java.awt.event.KeyListener;
 
 public class Manager implements KeyListener {
 	Board board;
+	private boolean isGaming;
+	//private boolean inBox;
 	private int key;
 	public Manager(){
 		board = new Board();
+		//inBox = true;
 		setUP();
+		isGaming = true;
 	}
 	public void setUP(){
 		board.panel.addKeyListener(this);
 	}public void playGame(){
-		//while();
-	}public void Graphic(){
-		
+		key = 37;
+		System.out.print(key);
+		while(isGaming){
+			graphic(key);
+			
+		}
+	}public void graphic(int direction){
+		int boxH = (int) board.boxH;
+		int boxW = (int) board.boxW;
+		if(direction == 37){//left
+			//System.out.print("Here");
+			if(Main.board[board.pacPosition[0]][board.pacPosition[1]-1] != 1  && Main.board[board.pacPosition[0]][board.pacPosition[1]-1] != 8){
+				//System.out.print("Here");
+				Main.board[board.pacPosition[0]][board.pacPosition[1]-1] = 3;
+				Main.board[board.pacPosition[0]][board.pacPosition[1]] = 2;
+				for(int i = 0; i <boxW; i++){
+					//System.out.println(direction);
+					//blank space
+					board.g.setColor(Color.YELLOW);
+					board.g.fillOval((int)Math.round((board.pacPosition[1] * boxW)) + 6 - i, (int)Math.round((board.pacPosition[0] * boxH)) + 5, 13, 13);
+					try {
+						Thread.sleep(28);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					board.g.setColor(Color.BLACK);
+					board.g.fillRect((int)Math.round((board.pacPosition[1] * boxW)+1) - i, (int)Math.round((board.pacPosition[0] * boxH) + 5), (int)Math.round(boxW)-1, (int)Math.round(boxH - ((boxH/5) * 2)));
+					
+				}board.display2();
+			}
+		}
+		else if(direction == 38){//up
+			
+		}
+		else if(direction == 39){//right
+			
+		}
+		else if(direction == 39){//right
+			
+		}  
 	}
 	@Override
 	public void keyPressed(KeyEvent arg0) {
